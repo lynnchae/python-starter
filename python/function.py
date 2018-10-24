00aea0e422a6ac03404b87185d963d8045f79b70
cailinfeng <cailinfeng@mobanker.com>
2018-10-25 00:09:28 +0800

    闭包 匿名函数

--- python/function.py
+++ python/function.py
@@ -62,3 +62,21 @@ extra = {'city': 'beijing', 'job': 'sweeper'}
 
 person('Jack', 19, city='beijing', job='sweeper')
 person('Jack', 19, **extra)
+
+
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+            return i * i
+
+        fs.append(f)
+    return fs
+
+
+f1, f2, f3 = count()
+
+print(f1())
+
+# 匿名函数
+print(list(map(lambda x: x * x, [1, 2, 3, 4])))
