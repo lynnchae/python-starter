b81db29e1c5a40af5d552ce94bf1e6917f28ce1e
cailinfeng <cailinfeng@mobanker.com>
2018-10-29 17:44:38 +0800

    小说爬虫完善

--- org/lynn/python/novelCrawler.py
+++ org/lynn/python/novelCrawler.py
@@ -3,22 +3,22 @@
 
 __author__ = 'Cailinfeng'
 
+import sys, time, os, requests
 from bs4 import BeautifulSoup
-import requests
 
 domain = 'https://www.biqukan.com'
 novel_fanren = 'https://www.biqukan.com/3_3050/'
-novels = []
+novel_chaper_urls = []
 nums = 0
 
 
 class Novel(object):
 
-    def __init__(self, novel_url):
-        self.novel_url = novel_url
+    def __init__(self, novel_chapter_url):
+        self.novel_chapter_url = novel_chapter_url
 
     def get_novel_content(self):
-        req = requests.get(url=self.novel_url)
+        req = requests.get(url=self.novel_chapter_url)
         html = req.content
         bf = BeautifulSoup(html, features="html.parser")
         texts = bf.find_all('div', attrs={'class': 'showtxt'})
@@ -27,26 +27,44 @@ class Novel(object):
 
     def save(self):
         self.get_novel_content()
-        with open('./txt/' + self.title + '.txt', 'w', encoding='utf-8') as f:
-            f.writelines(n.content)
+        with open('./txt/' + self.title + '.txt', 'a', encoding='utf-8') as f:
+            f.writelines(self.content)
             f.write('\n\n')
 
 
-def get_all_novels(novel):
-    req = requests.get(url=novel)
-    div_bf = BeautifulSoup(req.content, features="html.parser")
-    div = div_bf.find_all('div', attrs={'class': 'listmain'})
-    a_bf = BeautifulSoup(str(div[0]), features="html.parser")
-    a = a_bf.find_all('a')
-    global nums
-    nums = a[12:]
-    for each in a[12:22]:
-        novel = Novel(domain + each.get('href'))
-        yield novel
+class Novels(object):
+
+    def __init__(self, novel_url):
+        req = requests.get(url=novel_url)
+        div_bf = BeautifulSoup(req.content, features="html.parser")
+        div = div_bf.find_all('div', attrs={'class': 'listmain'})
+        a_bf = BeautifulSoup(str(div[0]), features="html.parser")
+        a = a_bf.find_all('a')
+        global nums
+        nums = a[12:]
+        for each in a[12:22]:
+            novel_chaper_urls.append(domain + each.get('href'))
+        nums = len(novel_chaper_urls)
+
+
+def progress(percent, width=50):
+    '''进度打印功能'''
+    if percent >= 100:
+        percent = 100
+    show_str = ('[%%-%ds]' % width) % (int(width * percent / 100) * "#")  # 字符串拼接的嵌套使用
+    sys.stdout.write('\r%s %d%%' % (show_str, percent))
+    sys.stdout.flush()
 
 
 if __name__ == '__main__':
+    for x in [z for z in os.listdir('./txt/') if os.path.isfile('./txt/'+z) and os.path.splitext(z)[1] == '.txt']:
+        os.remove('./txt/' + x)
     print('《凡人修仙传》前十章开始下载：')
-    for n in get_all_novels(novel_fanren):
-        n.save()
+    time.sleep(20)
+    Novels(novel_fanren)
+    for index, url in enumerate(novel_chaper_urls):
+        novel = Novel(url)
+        novel.save()
+        progress(int(100 * (index + 1) / nums), width=30)
+    print('\n')
     print('《凡人修仙传》前十章下载完成')
