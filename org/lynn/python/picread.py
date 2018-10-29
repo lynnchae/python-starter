e2a7dc747aceacf8be078fab544b9252dbbc4e40
cailinfeng <cailinfeng@mobanker.com>
2018-10-29 10:37:45 +0800

    图片调整

--- org/lynn/python/picread.py
+++ org/lynn/python/picread.py
@@ -6,5 +6,5 @@ __author__ = 'Cailinfeng'
 import pytesseract
 from PIL import Image
 
-im = Image.open(r'c.jpg')
+im = Image.open(r'./pic/e.jpg')
 print(pytesseract.image_to_string(im, lang='chi_sim'))
